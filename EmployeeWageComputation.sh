7cc9d59bf84b7cf6f8fba21021ebfa75498549ae
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-06 19:20:08 +0530

    Added: Calculation of PartTimeWages

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -3,14 +3,25 @@
 WAGE_PER_HOUR=20
 FULLDAY_WORKING_HOUR=8
 HALFDAY_WORKING_HOUR=4
+isFullDay=1
+isHalfDay=0
 echo "Welcome to Employee Wage Computation"
 
 presenty=$((RANDOM%2))
 if [ $presenty -eq 1 ]
 then
-	dailyWage=$(( $WAGE_PER_HOUR * $WORKING_HOUR ))
 	echo "Employee Present"
-	echo $dailyWage
+	workingHours=$((RANDOM%2))
+	case $workingHours in
+		$isFullDay)
+			fullDayWage=$(( $WAGE_PER_HOUR * $FULLDAY_WORKING_HOUR )) 
+			echo $fullDayWage
+			;;
+		$isHalfDay)
+			halfDayWage=$(( $WAGE_PER_HOUR * $HALFDAY_WORKING_HOUR ))
+			echo $halfDayWage 
+			;;
+	esac
 else
 	echo "Employee Absent"
 fi
