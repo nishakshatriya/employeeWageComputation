bf910f0e83e2269e8e054b69c51c1bb29ac2ab27
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-07 16:41:10 +0530

    Added: Daily and Total Wages to Dictorary

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,17 +1,21 @@
-totalDays=$(($totalDays+1))#!/bin/bash/ -x
+#!/bin/bash -x
+totalDays=$(($totalDays+1))
 #CONSTANT
+declare -A dailyArr
+declare -A totalArr
+
 WORKING_DAYS_PER_MONTH=20
 FULLDAY_WORKING_HOUR=8
 HALFDAY_WORKING_HOUR=4
 
 #VARIABLES
+counter=0
 isFullDay=1
 isHalfDay=0
 monthlyWage=0
 totalHour=0
 totalDays=0
 WAGE_PER_HOUR=20
-counter=0
 echo "Welcome to Employee Wage Computation"
 
 function getWorkingHours(){
@@ -35,29 +39,43 @@ do
 		case $workingHours in
 			$FULLDAY_WORKING_HOUR)
 				fullDayWage=$(($WAGE_PER_HOUR*$FULLDAY_WORKING_HOUR))
-				dailyArr[((counter++))]=$fullDayWage
+				dailyArr["Day$counter"]=$fullDayWage
 				totalWage=$(($totalWage+$fullDayWage))
-				totalArr[((counter++))]=$totalWage
+				totalArr["Day$counter"]=$totalWage
 
 				echo "Wage per day is:" $fullDayWage
 				totalDays=$(($totalDays+1))
 				;;
 			$HALFDAY_WORKING_HOUR )
 				halfDayWage=$(($WAGE_PER_HOUR*$HALFDAY_WORKING_HOUR))
-				dailyArr[((counter++))]=$halfDayWage
+				dailyArr["Day$counter"]=$halfDayWage
 				totalWage=$(($totalWage+$halfDayWage))
-				totalArr[((counter++))]=$totalWage
+				totalArr["Day$counter"]=$totalWage
 
 				echo "Wage per day is:" $halfDayWage
 				totalDays=$(($totalDays+1)) 
 				;;
 		esac
+		counter=$(( $counter+1 ))
+
 
 	else
 		echo "Employee Absent"
 	fi
-done
+	done
 
 echo "Daily Array-->" ${dailyArr[@]}
 echo "Total Array-->" ${totalArr[@]}
 
+for a in ${!dailyArr[@]}
+do
+	#echo "$a"
+	echo "$a ${dailyArr[$a]}"
+done
+
+echo "---------------------------"
+
+for b in ${!totalArr[@]}
+do
+	echo "$b ${totalArr[$b]}"
+done
