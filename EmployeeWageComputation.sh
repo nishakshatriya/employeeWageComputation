e9bda50b4cec3b0d48db87140c731b7f69ac36da
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-07 12:51:20 +0530

    Added: Wages for month

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,15 +1,33 @@
 #!/bin/bash/ -x
 
 WAGE_PER_HOUR=20
-WORKING_HOUR=8
+WORKING_DAYS_PER_MONTH=20
+FULLDAY_WORKING_HOUR=8
+HALFDAY_WORKING_HOUR=4
+isFullDay=1
+isHalfDay=0
+monthlyWage=0
 echo "Welcome to Employee Wage Computation"
 
-presenty=$((RANDOM%2))
-if [ $presenty -eq 1 ]
-then
-	dailyWage=$(( $WAGE_PER_HOUR * $WORKING_HOUR ))
-	echo "Employee Present"
-	echo $dailyWage
-else
-	echo "Employee Absent"
-fi
+for(( i=0; i<20; i++))
+do
+
+	presenty=$((RANDOM%2))
+	if [ $presenty -eq 1 ]
+	then
+		echo "Employee Present"
+		workingHours=$((RANDOM%2))
+		case $workingHours in
+			$isFullDay)
+				fullDayWage=$(($WAGE_PER_HOUR*$FULLDAY_WORKING_HOUR)) 
+				echo "Wage per day is:" $fullDayWage
+				;;
+			$isHalfDay)
+				halfDayWage=$(($WAGE_PER_HOUR*$HALFDAY_WORKING_HOUR))
+				echo "Wage per day is:" $halfDayWage 
+				;;
+		esac
+	else
+		echo "Employee Absent"
+	fi
+done
