bfc023bb4988bf68a7e40f6add32690e876a6651
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-07 13:28:20 +0530

    Added: Working Hours in the Condition

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash/ -x
+totalDays=$(($totalDays+1))#!/bin/bash/ -x
 
 WORKING_DAYS_PER_MONTH=20
 FULLDAY_WORKING_HOUR=8
@@ -6,10 +6,12 @@ HALFDAY_WORKING_HOUR=4
 isFullDay=1
 isHalfDay=0
 monthlyWage=0
-WAGE_PER_HOUR=80
+totalHour=0
+totalDays=0
+WAGE_PER_HOUR=25
 echo "Welcome to Employee Wage Computation"
 
-for(( i=0; i<20; i++))
+while [[ $totalHour -le 100 && $totalDays -le 20 ]]
 do
 
 	presenty=$((RANDOM%2))
@@ -19,14 +21,19 @@ do
 		workingHours=$((RANDOM%2))
 		case $workingHours in
 			$isFullDay)
+				totalHour=$(($totalHour+$FULLDAY_WORKING_HOUR))
 				fullDayWage=$(($WAGE_PER_HOUR*$FULLDAY_WORKING_HOUR))
 				echo "Wage per day is:" $fullDayWage
+				totalDays=$(($totalDays+1))
 				;;
 			$isHalfDay)
+				totalHour=$(($totalHour+$HALFDAY_WORKING_HOUR))
 				halfDayWage=$(($WAGE_PER_HOUR*$HALFDAY_WORKING_HOUR))
-				echo "Wage per day is:" $halfDayWage 
+				echo "Wage per day is:" $halfDayWage
+				totalDays=$(($totalDays+1)) 
 				;;
 		esac
+
 	else
 		echo "Employee Absent"
 	fi
