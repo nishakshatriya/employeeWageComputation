39a6e81a0181e1706ba1dbc8d7c405cd17192b6f
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-07 15:41:48 +0530

    Added:Storing DailyWages and TotalWages

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,14 +1,17 @@
 totalDays=$(($totalDays+1))#!/bin/bash/ -x
-
+#CONSTANT
 WORKING_DAYS_PER_MONTH=20
 FULLDAY_WORKING_HOUR=8
 HALFDAY_WORKING_HOUR=4
+
+#VARIABLES
 isFullDay=1
 isHalfDay=0
 monthlyWage=0
 totalHour=0
 totalDays=0
-WAGE_PER_HOUR=25
+WAGE_PER_HOUR=20
+counter=0
 echo "Welcome to Employee Wage Computation"
 
 function getWorkingHours(){
@@ -32,11 +35,19 @@ do
 		case $workingHours in
 			$FULLDAY_WORKING_HOUR)
 				fullDayWage=$(($WAGE_PER_HOUR*$FULLDAY_WORKING_HOUR))
+				dailyArr[((counter++))]=$fullDayWage
+				totalWage=$(($totalWage+$fullDayWage))
+				totalArr[((counter++))]=$totalWage
+
 				echo "Wage per day is:" $fullDayWage
 				totalDays=$(($totalDays+1))
 				;;
 			$HALFDAY_WORKING_HOUR )
 				halfDayWage=$(($WAGE_PER_HOUR*$HALFDAY_WORKING_HOUR))
+				dailyArr[((counter++))]=$halfDayWage
+				totalWage=$(($totalWage+$halfDayWage))
+				totalArr[((counter++))]=$totalWage
+
 				echo "Wage per day is:" $halfDayWage
 				totalDays=$(($totalDays+1)) 
 				;;
@@ -46,4 +57,7 @@ do
 		echo "Employee Absent"
 	fi
 done
+
+echo "Daily Array-->" ${dailyArr[@]}
+echo "Total Array-->" ${totalArr[@]}
 
